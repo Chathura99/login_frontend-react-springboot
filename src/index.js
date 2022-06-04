1cd0f6a9fce7b233cfa24685981a47736ed2cf26
Chathura <chathura.madura11@gmail.com>
2022-06-04 23:36:05 +0530

    <Provider store={store}>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
