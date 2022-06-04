582a854a23f6148b14b5aa0a0de6392bee033fdc
Chathura <chathura.madura11@gmail.com>
2022-06-04 23:35:36 +0530

    routing part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import Dashboard from "./pages/dashboard/Dashboard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage/>}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
