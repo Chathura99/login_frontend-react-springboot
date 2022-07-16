c4f1c0fa6d7a35714f04b89d9eb04ae04f856d29
Chathura <chathura.madura11@gmail.com>
2022-07-17 03:51:21 +0530

    fetch user details

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { fetchUserData } from "../../services/authenticationService";
 import "./dashboard.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { SidebarDataAdmin, SidebarDataMember } from "../../data/Data";
 import Topbar from "../../components/topbar/Topbar";
+import User from "../otherpages/User";
 
-const Dashboard = (props) => {
-  const [data, setData] = useState({});
-  const [otherdata, setOtherData] = useState({});
+const Dashboard = () => {
+  const [userdata, setData] = useState([]);
+  const [userrole, setUserRoles] = useState([]);
 
-  React.useEffect(() => {
-    fetchUserData()
-      .then((response) => {
-        console.log(response.data)
-        setData(response.data);
-        setOtherData(response.data.roles[0]);
-      })
-      .catch((e) => {
-        localStorage.clear();
-        // props.history.push('/');
-        window.location.href = "/";
-      });
+  useEffect(() => {    
+    userData();
   }, []);
+
+  const userData = async () => {
+    const res = await fetchUserData();
+    // console.log(res.data)
+    // console.log(res.data.roles)
+    setData(res.data);
+    setUserRoles(res.data.roles);
+  }
 
   const getSidebar = (role) => {
     if (role === "ADMIN") {
       return <Sidebar role={"Admin"} data={SidebarDataAdmin} />;
     } else if (role === "MEMBER") {
       return <Sidebar role={"Member"} data={SidebarDataMember} />;
-    }else{
+    } else {
       return <Sidebar role={"Other"} data={SidebarDataMember} />;
     }
   };
@@ -37,16 +36,34 @@ const Dashboard = (props) => {
     <>
       <Topbar />
       <div className="body">
-        <div className="sidebar">{getSidebar(otherdata.roleCode)}</div>
+        <div className="sidebar">
+          {getSidebar("ADMIN")}
+        </div>
 
         <div className="container">
           <div className="dashboard">
-            <h4>Hello {data && `${data.firstName} ${data.lastName}`}</h4>
-            <h6>User ID : {otherdata.id}</h6>
-            <h6>User Role : {otherdata.roleCode}</h6>
+            {/* print in string */}
+            <h3>Hello {userdata && `${userdata.firstName} ${userdata.lastName}`}</h3>
+            
+            
+
+            {
+            userrole.map((role, index) => (
+              <div key={role.id}>
+                <h6>User ID : </h6>
+                {role.id}
+                <h6>User Role : </h6>
+                {role.roleDescription}
+                <br />
+              </div>
+            ))
+            }
+            
+            <User />
           </div>
         </div>
       </div>
+      
     </>
   );
 };
