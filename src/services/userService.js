e0a09d1ab0a81434b37a0641c2ea1b8b4dd7aeb1
Chathura <chathura.madura11@gmail.com>
2022-07-17 03:52:08 +0530

    create getUser axios

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,17 @@
-// import axios from 'axios';
-// const USER_URL = "http://localhost:8080/api/v1";
+import axios from "axios";
+const USER_URL = "http://localhost:8080/api/v1/users";
 
-// export const getUsers = () => {
-//     return axios.get(USER_URL);
-// };
+const getToken = () => {
+  return localStorage.getItem("USER_KEY");
+};
+
+export const getUsers = () => {
+  // return axios.get(USER_URL);
+  return axios({
+    method: "GET",
+    url: USER_URL,
+    headers: {
+      Authorization: "Bearer " + getToken(),
+    },
+  });
+};
